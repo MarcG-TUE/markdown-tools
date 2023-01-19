e1c803058df5eac228f17d4088e6db6fe52bc8cb
Marc Geilen <m.c.w.geilen@tue.nl>
2023-01-19 07:55:48 +0100

    scalable header

---
+++ templates/presentation/dist/scripts/flexfont.js
@@ -0,0 +1,16 @@
+https://codepen.io/aknip/pen/GpGevp
+
+flexFont = function () {
+    var divs = document.getElementsByClassName("flexFont");
+    for(var i = 0; i < divs.length; i++) {
+        var relFontsize = divs[i].offsetWidth*0.05;
+        divs[i].style.fontSize = relFontsize+'px';
+    }
+};
+
+window.onload = function(event) {
+    flexFont();
+};
+window.onresize = function(event) {
+    flexFont();
+};
