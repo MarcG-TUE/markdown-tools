b1ed8ae260201e85bcabad2b8993efce7df4bd19
Marc Geilen <m.c.w.geilen@tue.nl>
2023-02-04 19:54:07 +0100

    convolve

--- questions/make.ps1
+++ questions/make.ps1
@@ -1,8 +1,9 @@
+if (-not (Test-Path -PathType Container ./dist)) {
+    New-Item -ItemType Directory -Force -Path ./dist
+}
+
 ../scripts/make-questions-ans -inputfile ./questions.md -outputfile ./questions.txt
 ../scripts/make-questions-latex -inputfile ./questions.md -outputfile ./questions.tex
 ../scripts/make-questions-html -inputfile ./questions.md -outputfile ./dist/questions.html
 
-if (-not (Test-Path -PathType Container ./dist)) {
-    New-Item -ItemType Directory -Force -Path ./dist
-}
 Copy-Item -Recurse -Force -Path ./styles -Destination ./dist/
