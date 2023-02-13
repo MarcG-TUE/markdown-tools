4cbc096c6ebb34235a2f8db39c95a12f611e459b
Marc Geilen <m.c.w.geilen@tue.nl>
2023-02-13 09:10:32 +0100

    sync

--- presentations/convolve-leuven-demo/make.ps1
+++ presentations/convolve-leuven-demo/make.ps1
@@ -1,2 +1,2 @@
 ../../scripts/make-presentation-reveal -inputfile ./presentation.md -outputdir ./dist
-../../scripts/make-presentation-ppt -inputfile ./presentation.md -template convolve.pptx
+../../scripts/make-presentation-ppt -inputfile ./presentation.md -template presentation.pptx
